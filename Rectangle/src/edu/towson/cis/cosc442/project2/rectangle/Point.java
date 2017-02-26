3bd418f542376e8429f7774ca8ccb4b21f37d9cd
Brian Knauff <bknauff@users.noreply.github.com>
2017-02-26 17:19:14 -0500

    Bug Fix
    Fixed a bug in Point.java class where constructor assignment of X was actually Y. Added comment in code to show where bug was and also show old code. 
    Ran Junit Test and all pass and also looked at coverage and JUnit test covers 100% of the code.

--- Rectangle/src/edu/towson/cis/cosc442/project2/rectangle/Point.java
+++ Rectangle/src/edu/towson/cis/cosc442/project2/rectangle/Point.java
@@ -15,7 +15,20 @@ public class Point {
 	 * @param y the y
 	 */
 	Point(Double x, Double y) {
+		/**
+		 * Constructor assignment of the x should be x but it was y (BUG IN THE CODE)
 		this.x = y;
+		this.y = y;
+		*/
+		this.setX(x);
+		this.setY(y);
+	}
+	
+	public void setX(Double x){
+		this.x = x;
+	}
+
+	public void setY(Double y){
 		this.y = y;
 	}
 }
